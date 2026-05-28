761b050e95b21c4b154ae833e7c303cd8a1ebd6a
Will Gislason <8203830+gislawill@users.noreply.github.com>
2026-05-28 10:16:21 -0500

    format fix and type fix

--- frontend/src/components/Common/ChartFormComponents/ChartLocationSelector.tsx
+++ frontend/src/components/Common/ChartFormComponents/ChartLocationSelector.tsx
@@ -190,7 +190,7 @@ function ChartLocationSelector({
           label={t('Admin 1')}
           value={selectedAdmin1Area?.adminCode ?? ''}
           SelectProps={{
-            renderValue: renderAdmin1Value,
+            renderValue: (value: unknown) => renderAdmin1Value(value as string),
           }}
           onChange={handleAdmin1Change}
           variant="outlined"
@@ -211,7 +211,8 @@ function ChartLocationSelector({
             label={t('Admin 2')}
             value={selectedAdmin2Area?.adminCode ?? ''}
             SelectProps={{
-              renderValue: renderAdmin2Value,
+              renderValue: (value: unknown) =>
+                renderAdmin2Value(value as string),
             }}
             onChange={handleAdmin2Change}
             variant="outlined"
@@ -231,7 +232,8 @@ function ChartLocationSelector({
             label={t('Admin 3')}
             value={selectedAdmin3Area?.adminCode ?? ''}
             SelectProps={{
-              renderValue: renderAdmin3Value,
+              renderValue: (value: unknown) =>
+                renderAdmin3Value(value as string),
             }}
             onChange={handleAdmin3Change}
             variant="outlined"
